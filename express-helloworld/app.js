093e2b570f36162ecb5b924508660ec6ccd3ab41
Bino Manjasseril <bino75@gmail.com>
2020-08-15 18:36:49 -0400

    bino commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('This is Bino version of app!\n');
 });
 
 app.listen(8080, function () {
